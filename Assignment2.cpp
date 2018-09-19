73f8b7cb044bdf33a755fa62d75f9a326628446a
Sean Warnock <swarnock@warnocksolutions.com>
2018-09-19 01:31:37 -0700

    Function removal

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -2,13 +2,15 @@
 Sean Warnock
 CSCI 15 Fall 2018
 
+http://www.chabotcollege.edu/faculty/kmehl/csci15/WordCount2.pdf
+
 Write a C++ program that reads lines of text from a file using the ifstream getline()method, tokenizes the lines into words ("tokens")using strtok(), and keeps statistics on the data in the file.  Your input and output file names will be supplied to your program on the command line, which you will access using argc and argv[].
 You need to count the total number of words seen, the number of unique words, the count of each individual word, and the number of lines.
 By "unique words",I just mean words that have not been seen yetin the program.  Once a word has been seen, it is no longer unique if or when you see it again.
 Also, remember and print the longest and shortest words in the file.  If there is a tie for longest or shortest word, you may resolve the tie in any consistent manner (e.g., use either the first one or the last one found, but use the same method for both longest and shortest).
 */
 #include <iostream>
-#include <string>
+#include <cstring>
 //#include <iomanip>
 #include <fstream>
 //#include <math.h>
@@ -23,41 +25,37 @@ Also, remember and print the longest and shortest words in the file.  If there i
 
 using namespace std;
 
-int ReadFile()
+int main()
 {
     string stringInFileName = "test.txt";
     ifstream inFile;
+    string stringOutFileName = "testout.txt";
+    ofstream outFile;
     char LineString[100];
 
-    //cout << "Please enter a file name in the local path to open. : ";
-    //cin >> stringInFileName;
-    inFile.open(stringInFileName);
 
-    cout << 'Got readfile' << endl;
-    while (!inFile.eof())
-    {
-        inFile.getline(LineString,100,'\n');
-        //intfigetline(inFile, LineString);
-        cout << LineString;
-    }
-
-    return 0;
-}
-
-int OutFile()
-{
-    string stringOutFileName;
-    ofstream outFile;
-
-    cout << "Please enter a file name in the local path to write. : ";
-    cin >> stringOutFileName;
-    //ofsteam
-}
-
-int main()
-{
     SetConsoleTitle(ASSIGNMENT);
     system("cls");
 
-    ReadFile();
+     cout << "Please enter a file name in the local path to open. : ";
+    //cin >> stringInFileName;
+    cout << endl;
+    cout << "Please enter a file name in the local path to write. : ";
+    //cin >> stringOutFileName;
+    cout << endl;
+
+    inFile.open(stringInFileName);
+    outFile.open(stringOutFileName);
+
+    while (!inFile.eof())
+    {
+        inFile.getline(LineString,100,'\n');
+        strtok(LineString, ' ');
+//need to test for "last line"
+        cout << LineString << endl;
+        outFile << LineString << endl;
+    }
+
+    inFile.close();
+    outFile.close();
 }
